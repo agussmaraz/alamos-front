788f8f2e6c85e463e188074e5c27f3d871a1e78a
Agustina Maraz <agus_maraz@hotmail.com>
2021-02-16 16:13:54 -0300

    Login

--- store/login.js
+++ store/login.js
@@ -1,24 +1,29 @@
 export const state = () => {
-  return {
-    datos: {
-      cedula: '',
-      contraseña: '',
-    },
-  }
-}
+    return {
+        datos: {
+            cedula: '',
+            contraseña: '',
+        },
+    };
+};
 
 export const actions = {
-  setData({ commit }, data) {
-    commit('SET_DATA', data)
-  },
-}
+    setData({ commit }, data) {
+        commit('SET_DATA', data);
+    },
+    login({ commit }, data) {
+        // const objeto = {};
+        // objeto.cedula = data.cedula,
+        // objeto.password = data.contraseña,
+    },
+};
 export const mutations = {
-  SET_DATA(state, data) {
-    for (const key in data) {
-      if (Object.hasOwnProperty.call(data, key)) {
-        const propiedad = data[key]
-        state.datos[key] = propiedad
-      }
-    }
-  },
-}
+    SET_DATA(state, data) {
+        for (const key in data) {
+            if (Object.hasOwnProperty.call(data, key)) {
+                const propiedad = data[key];
+                state.datos[key] = propiedad;
+            }
+        }
+    },
+};
